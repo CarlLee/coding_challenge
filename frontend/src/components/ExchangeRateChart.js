cfe50433dfdbe046cdb96152fd4d645156d91f19
Carl Lee <ljbha007@gmail.com>
2018-11-12 19:51:20 +0800

    Update README

--- frontend/src/components/ExchangeRateChart.js
+++ frontend/src/components/ExchangeRateChart.js
@@ -10,7 +10,7 @@ class ExchangeRateChart extends Component {
         labels: [],
         datasets: [
           {
-            label: "My First dataset",
+            label: "ETH to USD exchange rate",
             fillColor: "rgba(220,220,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
